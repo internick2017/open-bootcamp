716d2b27feb1a004ee281be567a3e7149f03bde6
Nick Granados <internick2017@gmail.com>
2022-05-28 10:26:43 -0300

    Ejercicios tema 4

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,61 @@
 public class Main {
     public static void main(String[] args) {
-
-        int result = functionTres(2, 6, 8);
+        //        Ejercicios tema 3
+        int result = functionTres(2, 8, -20);
         Coche miCoche = new Coche();
         miCoche.AdicionarPuerta();
         System.out.println(result);
         System.out.println(miCoche.puertas);
+//        Final Ejercicio tema 3
+        //        Ejercicios tema 4
+        String resultado;
+        if (result > 0) {
+            resultado = "positivo";
+            System.out.println(resultado);
+
+        } else {
+            resultado = "negativo";
+            System.out.println(resultado);
+        }
+        int number_while = 0;
+        while (number_while < 3) {
+            number_while++;
+            System.out.println(number_while);
+        }
+
+        do {
+            System.out.println(number_while);
+            break;
+        } while (number_while == 3);
+
+
+        for (int numeroFor = 0; numeroFor <= 3; numeroFor++) {
+            System.out.println(numeroFor);
+        }
+
+        String estacion = "verano";
+        switch (estacion){
+
+            case "verano":
+                    System.out.println("Ir a la playa");
+                    break;
+            case "invierno":
+                System.out.println("No bañarse");
+                break;
+            case "otoño":
+                System.out.println("Salir a pasear");
+                break;
+            case "primavera":
+                System.out.println("Trabajar");
+                break;
+            default:
+                System.out.println("Dormir");
+                break;
+        }
+
     }
 
+    // Inicio  Ejercicios tema 3
     public static int functionTres(int n1, int n2, int n3) {
         return n1 + n2 + n3;
     }
@@ -19,5 +67,6 @@ public class Main {
             this.puertas++;
         }
     }
+    //    Final Ejercicio tema 3
 }
 
